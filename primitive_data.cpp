24823b5f075fa0a77e4e268f5e0fbb3c90c54d5e
Phaitemc1982 <Vasquez.M051861@stu.sanjac.edu>
2024-08-25 22:54:44 +0000

    Practice commit

---
+++ primitive_data.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age = 20; // integer data type
+    float height = 5.9; // floating-point data type
+    double distance = 12345.6789; // double-precision floating-point data type
+    char grade = 'A'; // character data type
+    bool isPassed = true; //boolean data type
+
+    cout << "Age: "<< age << endl;
+    cout << "Height: " <<height << endl;
+    cout << "Distance: " << distance << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Passed: " << isPassed << endl;
+
+    return 0;
+}
